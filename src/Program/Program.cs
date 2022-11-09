20cddb37f8a03906d23c343d552e46025dc74c52
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2022-11-09 19:25:30 -0300

    Agregado tuti

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,14 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Node<Persona> n1 = new Node<Persona>(1, "coco", 21);
+            Node<Persona> n2 = new Node<Persona>(2, "julio", 76);
+            Node<Persona> n3 = new Node<Persona>(3, "roberto", 41);
+            Node<Persona> n4 = new Node<Persona>(4, "maria", 23);
+            Node<Persona> n5 = new Node<Persona>(5, "julian", 54);
+            Node<Persona> n6 = new Node<Persona>(6, "huber", 75);
+            Node<Persona> n7 = new Node<Persona>(7, "pepe julian onzima", 11);
+            Node<Persona> n8 = new Node<Persona>(8, "ultimo", 11);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -23,7 +24,18 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+            n3.AddChildren(n8);
 
+
+            AgeVisitor coso = new AgeVisitor();
+            OldestVisitor mayor = new OldestVisitor();
+            LongestNameVisitor largo = new LongestNameVisitor();
+            coso.Visit(n1);
+            mayor.Visit(n1);
+            largo.Visit(n1);
+            Console.WriteLine(coso);
+            Console.WriteLine(mayor);
+            Console.WriteLine(largo);
             // visitar el árbol aquí
         }
     }
